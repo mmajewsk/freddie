e6290e8006752c0e3515b579cbd8fe5148637b3e
mmajewsk <5279578+mmajewsk@users.noreply.github.com>
2024-03-03 18:58:35 +0000

    Deploying to gh-pages from @ mmajewsk/freddie@7d2991f9d924727fa7750870b470d367ac143535 🚀

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <guitar-fretboard></guitar-fretboard>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import GuitarFretboard from './components/GuitarFretboard.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    GuitarFretboard
+  }
+});
+</script>
